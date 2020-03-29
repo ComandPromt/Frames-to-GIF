f92f997ab6cf8e10ca699fa796d6f1731a1fb733
ComandPromt <vayase_a_ferrol@hotmail.com>
2020-03-29 21:02:06 +0200

    Update crear_gif.php

--- crear_gif.php
+++ crear_gif.php
@@ -1,18 +1,25 @@
 <?php
-session_start();
 
 date_default_timezone_set('Europe/Madrid');
 
 function comprobar_formatos($path){
-
+	
     $formatos = array(".jpg", "jpeg", ".png");
-
+	
     $dir = opendir($path);
+	
     $files = array();
+	
+	 $files =showFiles($path . '/');
+	 
     while ($current = readdir($dir)) {
+
         if ($current != "." && $current != "..") {
-            if (is_dir($path . $current)) {
-                showFiles($path . $current . '/');
+
+            if (is_dir($path ."/".$current)) {
+
+                $files =showFiles($path ."/". $current . '/');
+				
             } else {
                 if ($files[0] != 'error') {
                     if (!in_array(substr($current, -4), $formatos)) {
@@ -22,20 +29,23 @@ function comprobar_formatos($path){
             }
         }
     }
+	
+	sort($files);
+	
     return $files;
 }
 
-function redimensionarJPG($max_ancho, $max_alto, $ruta)
-{
-
+function redimensionarJPG($max_ancho, $max_alto, $ruta){
+	
     $imagenes = check_images_ext($ruta, "jpg");
-
     $calidad = 100;
+	
     for ($x = 0; $x < count($imagenes); $x++) {
-
+		
         $salida = $ruta . "/" . $imagenes[$x];
-
+		
         if (substr($imagenes[$x], -4, 1) == ".") {
+			
             $img_original = imagecreatefromjpeg("./" . $ruta . "/" . $imagenes[$x]);
             list($ancho, $alto) = getimagesize("./" . $ruta . "/" . $imagenes[$x]);
             //Se calcula ancho y alto de la imagen final
@@ -49,72 +59,101 @@ function redimensionarJPG($max_ancho, $max_alto, $ruta)
     }
 }
 
-function check_images_ext($path, $extension)
-{
+function check_images_ext($path, $extension){
+	
     $dir = opendir($path);
     $files = array();
+	
     while ($current = readdir($dir)) {
+		
         if ($current != "." && $current != "..") {
+			
             if (is_dir($path . $current)) {
                 showFiles($path . $current . '/');
-            } else {
+            }
+			
+			else {
+				
                 if (substr($current, -3) == $extension) {
                     $files[] = $current;
                 }
             }
         }
     }
+	
+	sort($files);
+	
     return $files;
 }
 
-function showFiles($path)
-{
+function showFiles($path){
+	
     $dir = opendir($path);
+	
     $files = array();
+	
     while ($current = readdir($dir)) {
+
         if ($current != "." && $current != "..") {
+			
             if (is_dir($path . $current)) {
                 showFiles($path . $current . '/');
-            } else {
+            }
+			
+			else {
+				
                 if (substr($current, -4, 1) == "." || substr($current, -3, 1) == "." || substr($current, -5, 1) == ".") {
-                    $files[] = $current;
+                
+				 $files[] = $current;
                 }
             }
         }
     }
+	
+	sort($files);
+	
     return $files;
 }
 
-function check_images($path)
-{
+function check_images($path){
+	
     $dir = opendir($path);
+	
     $files = array();
+	
     while ($current = readdir($dir)) {
+		
         if ($current != "." && $current != "..") {
+			
             if (is_dir($path . $current)) {
                 showFiles($path . $current . '/');
-            } else {
+            }
 
+			else {
                 $files[] = $current;
             }
         }
     }
+	
+	sort($files);
+	
     return $files;
 }
 
-function jpeg_jpg(array $imagenes, $ruta)
-{
+function jpeg_jpg(array $imagenes, $ruta){
+	
     for ($x = 0; $x < count($imagenes); $x++) {
+		
         if (substr($current, -4) == "jpeg") {
             rename($ruta . "/" . $imagenes, $ruta . "/" . substr($imagenes, 0, -4) . ".jpg");
         }
     }
 }
 
-function png_a_jpg($ruta)
-{
+function png_a_jpg($ruta){
+	
     $imagenes = check_images_ext($ruta, "png");
-
+	
     for ($x = 0; $x < count($imagenes); $x++) {
         $jpg = $ruta . "/" . substr($imagenes[$x], 0, -3) . "jpg";
         $image = imagecreatefrompng($ruta . "/" . $imagenes[$x]);
@@ -123,76 +162,72 @@ function png_a_jpg($ruta)
     }
 }
 
-$comprobacion = comprobar_formatos("img");
+$borrar = showFiles("img/");
+$num_imagenes=count($borrar);
 
-if ($comprobacion[0] != 'error') {
-	$imagenes = check_images("img");
-    jpeg_jpg($imagenes, "img");
-    png_a_jpg("img");
+if($num_imagenes<=170){
+	
+	$comprobacion = comprobar_formatos("img");
 
-    $imagenes = check_images_ext("img", "jpg");
-    $ancho = array();
-    $alto = array();
-
-    if (check_images_ext("img", "jpg") != null && check_images_ext("img", "jpg") != "" || check_images_ext("img", "png") != null && check_images_ext("img", "png") != "") {
-
-        for ($x = 0; $x < count($imagenes); $x++) {
-
-            $imagen = getimagesize("img/" . $imagenes[$x]);
-            $ancho[] = $imagen[0];
-            $alto[] = $imagen[1];
-        }
-
-        $anchofinal = min($ancho);
-        $altofinal = min($alto);
-
-        if ($anchofinal > 640) {
-            $anchofinal = 640;
-        }
-        if ($altofinal > 480) {
-            $altofinal = 480;
-        }
-
-        redimensionarJPG($anchofinal, $altofinal, "img");
-
-        if (count($imagenes) > 1) {
-
-            require "src/GifCreator/AnimGif.php";
-
-            $anim = new GifCreator\AnimGif();
-            if (count($imagenes) <= 10) {
-                $rapidez = 20;
-            } else {
-                $rapidez = 5;
-            }
-            $archivo = date("Y") . "_" . date("d") . "_" . date("m") . "_" . date("H") . "-" . date("i") . "-" . date("s");
-            $anim->create("img/", array($rapidez))
-                ->save('Output/' . $archivo . '.gif');
-
-            header("Content-disposition: attachment; filename=Output/$archivo.gif");
-            header("Content-type: MIME");
-            readfile("Output/$archivo.gif");
-			
-			if(isset($_SESSION['acceso']) && $_SESSION['acceso']){
-				            unlink('Output/' . $archivo . '.gif');
+	if ($comprobacion[0] != 'error') {
+		
+		$imagenes = check_images("img");
+		jpeg_jpg($imagenes, "img");
+		png_a_jpg("img");
+		$imagenes = check_images_ext("img", "jpg");
+		$ancho = array();
+		$alto = array();
+		
+		if ($imagenes != null && $imagenes != "" || check_images_ext("img", "png") != null && check_images_ext("img", "png") != "") {
+	
+			for ($x = 0; $x < count($imagenes); $x++) {
+				$imagen = getimagesize("img/" . $imagenes[$x]);
+				$ancho[] = $imagen[0];
+				$alto[] = $imagen[1];
 			}
-
-            print '<h2 name="imagen">' . date("Y") . "_" . date("d") . "_" . date("m") . "_" . date("H") . "-" . date("i") . "-" . date("s") . ".gif" . '</h2>';
-        }
-    } else {
-        print '<h1 name="salida">Folder empty</h1>';
-        print '<h2 name="imagen"></h2>';
-    }
+			
+			$anchofinal = min($ancho);
+			$altofinal = min($alto);
+			
+			if ($anchofinal > 640) {
+				$anchofinal = 640;
+			}
+			
+			if ($altofinal > 480) {
+				$altofinal = 480;
+			}
+			
+			redimensionarJPG($anchofinal, $altofinal, "img");
+			
+			if (count($imagenes) > 1) {
+				require "src/GifCreator/AnimGif.php";
+				$anim = new GifCreator\AnimGif();
+				if (count($imagenes) <= 10) {
+					$rapidez = 20;
+				} else {
+					$rapidez = 5;
+				}
+				$archivo = date("Y") . "_" . date("d") . "_" . date("m") . "_" . date("H") . "-" . date("i") . "-" . date("s");
+				$anim->create("img/", array($rapidez))
+					->save('Output/' . $archivo . '.gif');
+			}
+		}
+		
+		else {
+			print '<h1 name="salida">Folder empty</h1>';
+		}
+	}
 }
 
 $borrar = showFiles("img/");
 
-if ($borrar > 0) {
+if ($num_imagenes > 0) {
+	
     for ($x = 0; $x < count($borrar); $x++) {
-        print "img/" . $borrar[$x];
         unlink("img/" . $borrar[$x]);
     }
 }
 
 header('Location:index.php');
+
 ?>
